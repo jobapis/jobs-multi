53b0e3eed0b76f4d4c9fe8d5f958d2206e43c739
Karl <khughes.me@gmail.com>
2016-09-04 20:53:35 -0500

    Adding skeleton for JobsMulti methods

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# jobs-multi
-Making it easy to get jobs from multiple job boards at once
+# Jobs Multi
+Making it easy to get jobs from multiple job boards at once.
+
+## Usage
+```
+$providers = [
+    'Indeed' => [
+        'developerKey' => 'XXX',
+    ],
+    'Dice' => [],
+];
+
+$client = new JobsMulti($providers);
+
+$indeedJobs = $client->setLocation('chicago, il')
+    ->setKeyword('training')
+    ->setPage('1')
+    ->setPerPage('10')
+    ->getIndeedJobs();
+
+$diceJobs = $client
+    ->getDiceJobs();
+```
---
+++ tests/src/JobsMulti.php
@@ -0,0 +1,51 @@
+<?php namespace JobApis\Jobs\Client;
+
+
+class JobsMulti
+{
+
+    public function __construct($providers = [])
+    {
+        // TODO: Implement __construct() method.
+    }
+
+    public function getCareerbuilderJobs()
+    {
+
+    }
+
+    public function getDiceJobs()
+    {
+
+    }
+
+    public function getGovtJobs()
+    {
+
+    }
+
+    public function getIndeedJobs()
+    {
+
+    }
+
+    public function setKeyword($keyword)
+    {
+
+    }
+
+    public function setLocation($location)
+    {
+
+    }
+
+    public function setPage($page)
+    {
+
+    }
+
+    public function setPerPage($perPage)
+    {
+
+    }
+}
