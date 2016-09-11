b109421a5ebe0b801582c394fed0dcc37d793018
Karl <khughes.me@gmail.com>
2016-09-11 13:20:09 -0500

    Adding tests for keyword, location setter

--- src/JobsMulti.php
+++ src/JobsMulti.php
@@ -1,41 +1,16 @@
 <?php namespace JobApis\Jobs\Client;
 
 use JobApis\Jobs\Client\Providers\CareerbuilderProvider;
-use JobApis\Jobs\Client\Queries\CareerbuilderQuery;
-use JobApis\Jobs\Client\Queries\DiceQuery;
-use JobApis\Jobs\Client\Queries\GovtQuery;
-use JobApis\Jobs\Client\Queries\IndeedQuery;
-use JobApis\Jobs\Client\Collection;
+use JobApis\Jobs\Client\Queries\AbstractQuery;
 
 class JobsMulti
 {
     /**
-     * Careerbuilder query object
+     * Job board API query objects
      *
-     * @var CareerbuilderQuery
+     * @var AbstractQuery
      */
-    protected $careerbuilderQuery;
-
-    /**
-     * Dice query object
-     *
-     * @var DiceQuery
-     */
-    protected $diceQuery;
-
-    /**
-     * Government jobs query object
-     *
-     * @var GovtQuery
-     */
-    protected $govtQuery;
-
-    /**
-     * Indeed query object
-     *
-     * @var IndeedQuery
-     */
-    protected $indeedQuery;
+    protected $queries = [];
 
     /**
      * Creates query objects for each provider and creates this unified client.
@@ -47,53 +22,165 @@ class JobsMulti
         foreach ($providers as $provider => $options) {
             $query = $provider.'Query';
             $className = 'JobApis\\Jobs\\Client\\Queries\\'.$query;
-            $this->{lcfirst($query)} = new $className($options);
+            $this->addQuery(lcfirst($query), $className, $options);
         }
+    }
+
+    /**
+     * Instantiates a Query object and adds it to the queries array
+     *
+     * @param $key Query name
+     * @param $className Query class name
+     * @param array $options Array of parameters to add to constructor
+     */
+    public function addQuery($key, $className, $options = [])
+    {
+        $this->queries[$key] = new $className($options);
+        return $this;
     }
 
     /**
      * Gets jobs from Careerbuilder and hydrates a new jobs collection
      *
-     * @var Collection
+     * @return \JobApis\Jobs\Client\Collection
      */
     public function getCareerbuilderJobs()
     {
-        $provider = new CareerbuilderProvider($this->careerbuilderQuery);
+        $provider = new CareerbuilderProvider($this->queries['careerbuilderQuery']);
         return $provider->getJobs();
     }
 
+    /**
+     * Gets jobs from Dice and hydrates a new jobs collection
+     *
+     * @return \JobApis\Jobs\Client\Collection
+     */
     public function getDiceJobs()
     {
 
     }
 
+    /**
+     * Gets jobs from the Government Jobs API and hydrates a new jobs collection
+     *
+     * @return \JobApis\Jobs\Client\Collection
+     */
     public function getGovtJobs()
     {
 
     }
 
+    /**
+     * Gets jobs from Indeed and hydrates a new jobs collection
+     *
+     * @return \JobApis\Jobs\Client\Collection
+     */
     public function getIndeedJobs()
     {
 
     }
 
+    /**
+     * Sets a keyword on the query for each provider
+     *
+     * @param $keyword
+     *
+     * @return $this
+     */
     public function setKeyword($keyword)
     {
+        foreach ($this->queries as $classKey => $query) {
+            $className = ucfirst($classKey);
 
+            switch ($className) {
+                case 'CareerbuilderQuery':
+                    $query->set('Keywords', $keyword);
+                    break;
+                case 'DiceQuery':
+                    $query->set('text', $keyword);
+                    break;
+                case 'GovtQuery':
+                    $query->set('query', $keyword);
+                    break;
+                case 'GithubQuery':
+                    $query->set('search', $keyword);
+                    break;
+                case 'IndeedQuery':
+                    $query->set('q', $keyword);
+                    break;
+                default:
+                    throw new \Exception("Provider {$className} not found");
+            }
+        }
+        return $this;
     }
 
+    /**
+     * Sets a location on the query for each provider
+     *
+     * @param $location
+     *
+     * @return $this
+     */
     public function setLocation($location)
     {
+        if (!$this->isValidLocation($location)) {
+            throw new \OutOfRangeException("Location parameter must follow the pattern 'City, ST'.");
+        }
 
+        $locationArr = explode(', ', $location);
+        $city = $locationArr[0];
+        $state = $locationArr[1];
+
+        foreach ($this->queries as $classKey => $query) {
+            $className = ucfirst($classKey);
+
+            switch ($className) {
+                case 'CareerbuilderQuery':
+                    $query->set('UseFacets', 'true');
+                    $query->set('FacetCityState', $location);
+                    break;
+                case 'DiceQuery':
+                    $query->set('city', $city);
+                    $query->set('state', $state);
+                    break;
+                case 'GovtQuery':
+                    $queryString = $query->get('query').' in '.$location;
+                    $query->set('query', $queryString);
+                    break;
+                case 'GithubQuery':
+                    $query->set('location', $location);
+                    break;
+                case 'IndeedQuery':
+                    $query->set('l', $location);
+                    break;
+                default:
+                    throw new \Exception("Provider {$className} not found");
+            }
+        }
+        return $this;
     }
 
     public function setPage($page)
     {
-
+        return $this;
     }
 
     public function setPerPage($perPage)
     {
+        return $this;
+    }
 
+    /**
+     * Tests whether location string follows valid convention (City, ST)
+     *
+     * @param string $location
+     *
+     * @return bool
+     */
+    private function isValidLocation($location = null)
+    {
+        preg_match("/([^,]+),\s*(\w{2})/", $location, $matches);
+        return ($matches[1] && $matches[2]) ? true : false;
     }
 }
--- tests/src/JobsMultiTest.php
+++ tests/src/JobsMultiTest.php
@@ -7,25 +7,129 @@ class JobsMultiTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $providers = [
+        $this->providers = [
             'Careerbuilder' => [
                 'DeveloperKey' => 'YYY',
             ],
             'Dice' => [],
             'Govt' => [],
+            'Github' => [],
             'Indeed' => [
                 'publisher' => 'ZZZ',
             ],
         ];
-        $this->client = new JobsMulti($providers);
+        $this->client = new JobsMulti($this->providers);
     }
 
     public function testItCanInstantiateQueryObjects()
     {
-        $class = new \ReflectionClass('JobApis\Jobs\Client\JobsMulti');
-        $property = $class->getProperty('careerbuilderQuery');
-        $property->setAccessible(true);
-        // Gets the careerbuilderQuery property even though it's protected
-        var_dump($property->getValue($this->client)); exit;
+        $queries = $this->getProtectedProperty($this->client, 'queries');
+
+        foreach ($this->providers as $className => $provider) {
+            $key = lcfirst($className).'Query';
+            $this->assertTrue(isset($queries[$key]));
+            $this->assertEquals(
+                'JobApis\\Jobs\\Client\\Queries\\'.$className.'Query',
+                get_class($queries[$key])
+            );
+        }
     }
+
+    public function testItCanSetKeywordOnAllProviders()
+    {
+        $keyword = uniqid();
+        $result = $this->client->setKeyword($keyword);
+
+        $this->assertEquals(get_class($this->client), get_class($result));
+
+        $queries = $this->getProtectedProperty($this->client, 'queries');
+
+        foreach ($this->providers as $className => $provider) {
+            $key = lcfirst($className) . 'Query';
+            switch ($className) {
+                case 'Careerbuilder':
+                    $this->assertEquals($keyword, $queries[$key]->get('Keywords'));
+                break;
+                case 'Dice':
+                    $this->assertEquals($keyword, $queries[$key]->get('text'));
+                break;
+                case 'Govt':
+                    $this->assertEquals($keyword, $queries[$key]->get('query'));
+                break;
+                case 'Github':
+                    $this->assertEquals($keyword, $queries[$key]->get('search'));
+                    break;
+                case 'Indeed':
+                    $this->assertEquals($keyword, $queries[$key]->get('q'));
+                    break;
+                default:
+                    throw new \Exception("Provider {$className} not found in test.");
+            }
+        }
+    }
+
+    public function testItCanSetLocationOnAllProviders()
+    {
+        $city = uniqid();
+        $state = 'te';
+        $location = $city.', '.$state;
+        $result = $this->client->setLocation($location);
+
+        $this->assertEquals(get_class($this->client), get_class($result));
+
+        $queries = $this->getProtectedProperty($this->client, 'queries');
+
+        foreach ($this->providers as $className => $provider) {
+            $key = lcfirst($className) . 'Query';
+            switch ($className) {
+                case 'Careerbuilder':
+                    $this->assertEquals('true', $queries[$key]->get('UseFacets'));
+                    $this->assertEquals($location, $queries[$key]->get('FacetCityState'));
+                    break;
+                case 'Dice':
+                    $this->assertEquals($city, $queries[$key]->get('city'));
+                    $this->assertEquals($state, $queries[$key]->get('state'));
+                    break;
+                case 'Govt':
+                    $this->assertNotEquals(false, strpos($queries[$key]->get('query'), 'in '.$location));
+                    break;
+                case 'Github':
+                    $this->assertEquals($location, $queries[$key]->get('location'));
+                    break;
+                case 'Indeed':
+                    $this->assertEquals($location, $queries[$key]->get('l'));
+                    break;
+                default:
+                    throw new \Exception("Provider {$className} not found in test.");
+            }
+        }
+    }
+
+    public function testItCanSetPageOnAllProviders()
+    {
+        $page = rand(1, 20);
+        $result = $this->client->setPage($page);
+
+        $this->assertEquals(get_class($this->client), get_class($result));
+        // TODO: add more assertions for each provider
+    }
+
+    public function testItCanSetPerPageOnAllProviders()
+    {
+        $perPage = rand(1, 50);
+        $result = $this->client->setPerPage($perPage);
+
+        $this->assertEquals(get_class($this->client), get_class($result));
+        // TODO: add more assertions for each provider
+    }
+
+    private function getProtectedProperty($object, $property = null)
+    {
+        $class = new \ReflectionClass(get_class($object));
+        $property = $class->getProperty($property);
+        $property->setAccessible(true);
+
+        return $property->getValue($object);
+    }
+
 }
